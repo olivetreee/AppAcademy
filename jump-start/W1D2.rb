8cdc77478bca3f085ea75ec27233662b690b636f
Clapinton <eric.olivetree@gmail.com>
2016-07-21 09:25:33 -0700

    Pair exercise from W1D2

---
+++ jump-start/W1D2.rb
@@ -0,0 +1,88 @@
+# Puts While/Each
+# ----------------------------------------
+# Write a method, #puts_while, that accepts an array as an argument and will puts
+# every element in the array. Do this using a while loop.
+
+def puts_while(array)
+  i=0
+  while i<array.length
+    puts array[i]
+    i+=1
+  end
+end
+
+# Write a method, #puts_each, that accepts an array as an argument and will puts
+# every element in the array. Do this using Array's #each method
+
+def puts_each(array)
+  array.each do |element|
+    puts element
+  end
+end
+
+
+
+
+# Present Pet
+# ----------------------------------------
+# Write a method, #present_pet, that takes two arguments:
+# a pet name and an animal type, both strings. It should return a string
+# that announces the pet (e.g. if given "Todd" and "turtle", it
+# should return "Todd the turtle has arrived.").
+
+def present_pet(pet_name, animal)
+  return "#{pet_name} the #{animal} has arrived."
+end
+
+puts "---------Present Pet----------"
+puts present_pet("Sam", "salamander") == "Sam the salamander has arrived."
+puts present_pet("Roger", "rabbit") == "Roger the rabbit has arrived."
+puts present_pet("Catie", "canary") == "Catie the canary has arrived."
+
+
+
+
+######### SOLUTIONS #########
+
+# Puts While / Puts Each
+# ----------------------------------------
+# Write a method, #puts_while, that accepts an array as an argument and will puts
+# every element in the array. Do this using a while loop.
+
+def puts_while(array)
+  idx = 0
+  while idx < array.length
+    puts array[idx]
+    idx += 1
+  end
+end
+
+# Write a method, #puts_each, that accepts an array as an argument and will puts
+# every element in the array. Do this using Array's #each method
+
+def puts_each(array)
+  array.each do |element|
+    puts element
+  end
+end
+
+## Discussion topics:
+#   * Which of these loops reads more like English?
+
+
+
+
+# Present Pet
+# ----------------------------------------
+# Write a method, #present_pet, that takes two arguments:
+# a pet name and an animal type, both strings. It should return a string
+# that announces the pet (e.g. if given "Todd" and "turtle", it
+# should return "Todd the turtle has arrived.").
+
+def present_pet(pet_name, animal)
+  "#{pet_name} the #{animal} has arrived."
+end
+
+## Discussion topics
+#   * With strings, is either interpolation or concatenation better? Why?
+#   * Why is there no return statement?
