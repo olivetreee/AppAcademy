d6f8aa35398be95186a4b3976560f88b8c89688f
Clapinton <eric.olivetree@gmail.com>
2016-09-15 22:20:49 -0700

    Working queue class

--- homework/W1D4/queue.rb
+++ homework/W1D4/queue.rb
@@ -1,22 +1,47 @@
 class Queue
 
   def initialize
-
+    @elements = []
   end
 
 
-  def enqueue
-
+  def enqueue(el)
+    @elements.unshift(el)
   end
 
 
   def dequeue
-
+    @elements.pop
   end
 
 
   def show
-
+    p @elements
   end
 
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Writing my name:"
+  puts "END ---------------- START"
+  queueu = Queue.new
+  queueu.show
+  queueu.enqueue("C")
+  queueu.show
+  queueu.enqueue("I")
+  queueu.show
+  queueu.enqueue("R")
+  queueu.show
+  queueu.enqueue("E")
+  queueu.show
+  puts
+  puts "Deleting my name:"
+  queueu.dequeue
+  queueu.show
+  queueu.dequeue
+  queueu.show
+  queueu.dequeue
+  queueu.show
+  queueu.dequeue
+  queueu.show
 end
