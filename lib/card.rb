eb0b535a1c70b524e290c6adf2ff456c437a7c28
Clapinton <eric.olivetree@gmail.com>
2016-09-21 13:58:51 -0700

    Initial poker structure. Card class created

---
+++ lib/card.rb
@@ -0,0 +1,49 @@
+class Card
+  attr_reader :value, :suit
+
+  STRING_VALUES = {
+    "A" => "Ace",
+    "2" => "Two",
+    "3" => "Three",
+    "4" => "Four",
+    "5" => "Five",
+    "6" => "Six",
+    "7" => "Seven",
+    "8" => "Eight",
+    "9" => "Nine",
+    "10" => "Ten",
+    "J" => "Jack",
+    "Q" => "Queen",
+    "K" => "King"
+  }
+
+  def initialize(value, suit)
+    self.value = value
+    self.suit = suit
+  end
+
+  def value=(val)
+    raise "Invalid value" unless (0..12).cover?(val)
+    @value = valid_values[val]
+  end
+
+
+  def suit=(val)
+    unless [:heart, :diamond, :club, :spade].include?(val)
+      raise "Invalid suit"
+    end
+
+    @suit = val
+  end
+
+  def to_s
+    "#{STRING_VALUES[@value]} of #{@suit.to_s.capitalize}s"
+  end
+
+  private
+
+  def valid_values
+    ["A"] + (2..10).to_a.map(&:to_s) + ["J", "Q", "K"]
+  end
+
+end
---
+++ spec/card_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require 'card'
+
+describe Card do
+  let(:card) {Card.new(0, :heart)}
+
+  context "#initialize" do
+    it "initializes with a value and a suit" do
+      expect(card.value).to eq("A")
+      expect(card.suit).to eq(:heart)
+    end
+
+    it "raises an error if it receives bad parameters" do
+      expect{Card.new(-1, :heart)}.to raise_error("Invalid value")
+      expect{Card.new(5, :apps)}.to raise_error("Invalid suit")
+    end
+  end
+
+  context "#to_s" do
+    it "returns value and suit of the card" do
+      expect(card.to_s).to eq("Ace of Hearts")
+    end
+  end
+end
