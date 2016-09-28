c8eb0cd8d8755eeb37c04f3ad5044e01faed66ef
Clapinton <eric.olivetree@gmail.com>
2016-09-28 14:19:58 -0700

    create ShortenedUrl and User classes and associations.

---
+++ URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,26 @@
+class ShortenedUrl < ActiveRecord::Base
+  validates :short_url, uniqueness: true
+  validates :user_id, presence: true
+
+  belongs_to :submitter,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+  def self.random_code
+    shortened = SecureRandom::urlsafe_base64
+    shortened = SecureRandom::urlsafe_base64 while self.exists?(short_url: shortened)
+
+    shortened
+  end
+
+  def self.generate_short_url
+    "https://a.a/#{ShortenedUrl.random_code}/"
+  end
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    shortened = ShortenedUrl.generate_short_url
+
+    ShortenedUrl.create!(user_id: user.id, long_url: long_url, short_url: shortened)
+  end
+end
--- URLShortener/db/schema.rb
+++ URLShortener/db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928183616) do
+ActiveRecord::Schema.define(version: 20160928191443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shortened_urls", force: :cascade do |t|
+    t.string   "long_url",   null: false
+    t.string   "short_url"
+    t.integer  "user_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "shortened_urls", ["short_url"], name: "index_shortened_urls_on_short_url", unique: true, using: :btree
+  add_index "shortened_urls", ["user_id"], name: "index_shortened_urls_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at"
