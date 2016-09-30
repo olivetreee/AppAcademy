2cd165abd93600090df687fc1d3ee01e3fab9691
Clapinton <eric.olivetree@gmail.com>
2016-09-30 13:32:25 -0700

    Working ::columns and ::finalize

--- main_curriculum/w3/w3d5/active_record_lite/lib/01_sql_object.rb
+++ main_curriculum/w3/w3d5/active_record_lite/lib/01_sql_object.rb
@@ -1,22 +1,37 @@
+require 'byebug'
 require_relative 'db_connection'
 require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
+
   def self.columns
-    # ...
+    @cols ||= DBConnection.execute2(<<-SQL)
+      SELECT * FROM #{self.table_name}
+    SQL
+    @cols.first.map(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |col|
+      # byebug
+      define_method("#{col}=") do |val|
+        attributes #initializing @attributes if not already
+        @attributes[col]=val
+      end
+      define_method("#{col}") do
+        @attributes[col]
+      end
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name ||= self.name.tableize
   end
 
   def self.all
@@ -32,11 +47,11 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    # self.class.finalize!
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
