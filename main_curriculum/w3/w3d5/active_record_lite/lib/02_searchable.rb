a3475b929ef7958653e3ff1176f8c99e460722bc
Clapinton <eric.olivetree@gmail.com>
2016-09-30 16:04:25 -0700

    Searchable module working

--- main_curriculum/w3/w3d5/active_record_lite/lib/02_searchable.rb
+++ main_curriculum/w3/w3d5/active_record_lite/lib/02_searchable.rb
@@ -1,12 +1,26 @@
+require 'byebug'
 require_relative 'db_connection'
 require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    #grab the params keys as cols
+    #grab the params values as the values to be looked for
+
+    col_names = params.keys
+    query_paramaters = col_names.map do |col|
+      "#{col}='#{params[col]}'"
+    end
+    query = query_paramaters.join(" AND ")
+    results = DBConnection.execute(<<-SQL)
+      SELECT * FROM #{self.table_name}
+      WHERE #{query}
+    SQL
+    
+    results.map {|result| self.new(result)}
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
