08c27e27898ea865db2e17c0c7ee7bf0785e4015
Clapinton <eric.olivetree@gmail.com>
2016-09-29 19:20:45 -0700

    Homework for w3d3

---
+++ main_curriculum/w3/w3d3/people_houses/DemoProject/db/migrate/20160928055343_create_people_table.rb
@@ -0,0 +1,9 @@
+class CreatePeopleTable < ActiveRecord::Migration
+  def change
+    create_table :people do |t|
+      t.string :name, null: false
+      t.integer :house_id, null: false
+      t.timestamps
+    end
+  end
+end
