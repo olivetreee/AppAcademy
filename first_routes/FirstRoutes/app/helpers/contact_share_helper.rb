b097dce0b90e3a45a9304345ec12bdcfbd02daa6
Clapinton <eric.olivetree@gmail.com>
2016-10-03 20:08:08 -0700

    Preparing files for move

--- first_routes/FirstRoutes/app/helpers/contact_share_helper.rb
+++
@@ -1,2 +0,0 @@
-module ContactShareHelper
-end
--- first_routes/FirstRoutes/db/migrate/20161003222446_fix_users_uniqueness.rb
+++
@@ -1,7 +0,0 @@
-class FixUsersUniqueness < ActiveRecord::Migration
-  def change
-    remove_column :users, :username
-    add_column :users, :username, :string, null: false, default: "Guest"
-    # add_index :users, :username, unique: true
-  end
-end
