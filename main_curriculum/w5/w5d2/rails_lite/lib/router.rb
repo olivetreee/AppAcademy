ac5b8251cd48bcdd5af0d74814afdb3208271ebb
Clapinton <eric.olivetree@gmail.com>
2016-10-11 19:36:11 -0700

    Implementing #run. Not yet done.

---
+++ main_curriculum/w5/w5d2/rails_lite/lib/router.rb
@@ -0,0 +1,65 @@
+require 'byebug'
+
+class Route
+  attr_reader :pattern, :http_method, :controller_class, :action_name
+
+  def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern #URI pattern
+    @http_method = http_method #GET, POST, PUT, DELETE
+    @controller_class = controller_class #The controller name
+    @action_name = action_name #The method from the controller
+  end
+
+  # checks if pattern matches path and method matches request method
+  def matches?(req)
+    #req.path
+    #req.request_method (returns a upcase string)
+    #@http_method (is a downcase symbol)
+    req_method = req.request_method.downcase.to_sym
+    (@pattern =~ req.path) && (req_method == @http_method)
+  end
+
+  # use pattern to pull out route params (save for later?)
+  # instantiate controller and call controller action
+  def run(req, res)
+
+  end
+end
+
+class Router
+  attr_reader :routes
+
+  def initialize
+    @routes = []
+  end
+
+  # simply adds a new route to the list of routes
+  def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
+  end
+
+  # evaluate the proc in the context of the instance
+  # for syntactic sugar :)
+  def draw(&proc)
+  end
+
+  # make each of these methods that
+  # when called add route
+  [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
+  end
+
+  # should return the route that matches this request
+  def match(req)
+    @routes.find{|route| route.matches?(req)}
+  end
+
+  # either throw 404 or call run on a matched route
+  def run(req, res)
+    STOPPED HERE
+    controller = ControllerBase.new(req,res)
+    controller.invoke_action(req.request_method.downcase.to_sym)
+  end
+end
