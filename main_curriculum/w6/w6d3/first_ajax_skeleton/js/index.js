6a7b5842d19833d8535ef17db32f4f980aea4bbf
Clapinton <eric.olivetree@gmail.com>
2016-10-18 21:27:42 -0700

    Complete homework

--- main_curriculum/w6/w6d3/first_ajax_skeleton/js/index.js
+++ main_curriculum/w6/w6d3/first_ajax_skeleton/js/index.js
@@ -1,5 +1,14 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax ({
+  method: 'GET',
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  dataType: 'json',
+  success: function(response) {
+    console.log(response);
+  }
+})
 
 // Add another console log here, outside your AJAX request
+console.log("2nd console log")
