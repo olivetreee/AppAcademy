833333715c2373c02f6864edf843ea1a8ab75814
Clapinton <eric.olivetree@gmail.com>
2016-12-09 11:03:50 -0800

    Dijkstra2 solved

--- main_curriculum/w13/w13d4/dijkstras_skeleton/lib/dijkstra2.rb
+++ main_curriculum/w13/w13d4/dijkstras_skeleton/lib/dijkstra2.rb
@@ -2,5 +2,34 @@ require_relative 'graph'
 require_relative 'priority_map'
 
 # O(|V| + |E|*log(|V|)).
-def dijkstra2(source)
+def dijkstra2(source_node)
+
+  shortest_paths = {}
+  possible_paths = PriorityMap.new do |node1_data, node2_data|
+    node1_data[:cost] <=> node2_data[:cost]
+  end
+
+  possible_paths[source_node] = {cost: 0, previous: nil}
+
+  until possible_paths.empty?
+    current_node, node_data = possible_paths.extract
+    shortest_paths[current_node] = node_data
+
+    current_node.out_edges.each do |edge|
+      destination = edge.to_vertex
+
+      new_cost = edge.cost + shortest_paths[current_node][:cost]
+
+      # If I have the node on the possible_paths and the new_cost is bigger than the new cost, it doesn't make sense to update the cost. We only update it if:
+      # • the node is not on possible_paths (hence, we initialize it) OR
+      # • the new_cost is lower than the new cost
+
+      if (!possible_paths.has_key?(destination) || new_cost < possible_paths[destination][:cost])
+        possible_paths[destination] = {cost: new_cost, previous: current_node}
+      end
+    end
+  end
+
+  return shortest_paths
+
 end
--- main_curriculum/w13/w13d4/dijkstras_skeleton/lib/priority_map.rb
+++ main_curriculum/w13/w13d4/dijkstras_skeleton/lib/priority_map.rb
@@ -2,32 +2,53 @@ require_relative 'heap2'
 
 class PriorityMap
   def initialize(&prc)
+    @map = {}
+
+    prc ||= Proc.new{ |a, b| a <=> b }
+    @queue = BinaryMinHeap.new { |k1, k2| prc.call(@map[k1], @map[k2]) }
+
   end
 
   def [](key)
+    @map[key]
   end
 
   def []=(key, value)
+    if @map.has_key?(key)
+      self.update(key, value)
+    else
+      self.insert(key, value)
+    end
   end
 
   def count
+    @queue.count
   end
 
   def empty?
+    self.count === 0
   end
 
   def extract
+    key = @queue.extract
+    value = @map.delete(key)
+    [key, value]
   end
 
   def has_key?(key)
+    @map.has_key?(key)
   end
 
   protected
   attr_accessor :map, :queue
 
   def insert(key, value)
+    @map[key] = value
+    @queue.push(key)
   end
 
   def update(key, value)
+    @map[key] = value
+    @queue.reduce!(key)
   end
 end
